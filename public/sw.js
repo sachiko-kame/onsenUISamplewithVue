8ba0725b824667526921c900a83b1eed9079ac24
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2019-02-03 22:34:59 +0900

    一旦jsファイルを登録するまで。

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,14 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+
+  // リクエストされたすべての「.js」ファイルに一致するルートをWorkboxに登録することです
+  workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+
+    // 可能であればJavaScriptファイルをネットワークから取得し、ネットワークに障害が発生した場合はキャッシュされたバージョンにフォールバックする必要がある場合は、「ネットワーク優先」戦略を使用してこれを実現できます。
+    workbox.strategies.networkFirst()
+  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
